60f28bea915aa46cd843c407a9e9ecf898c5f9a9
konozca-juane-olivan <juane.olivan@konozca.com>
2026-01-14 13:51:13 +0100

    Latest version

--- docs/swagger-initializer.js
+++ docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://konozca-juane-olivan.github.io/Multiopticas-API-Swagger-Documentation/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
